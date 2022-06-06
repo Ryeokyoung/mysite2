6ade13046db7c1d975d2d250a362717670bb0bc1
Ryeokyoung <ryeokyoungk@gmail.com>
2022-06-07 01:31:03 +0900

    -

--- src/com/javaex/controller/UserController.java
+++ src/com/javaex/controller/UserController.java
@@ -18,10 +18,32 @@ public class UserController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("UserController");
 		
-		//포워드 (폼 포워드)
-		WebUtil.forword(request, response, "/WEB-INF/views/user/joinForm.jsp");
-	
+		//action 꺼냄 
+		String action = request.getParameter("action");
+		System.out.println(action);	
+		
+		
+		if("joinForm".equals(action)) {
+			//회원가입 폼 포워드
+			WebUtil.forword(request, response, "/WEB-INF/views/user/joinForm.jsp");
+		
+		}else if ("join".equals(action)) {//회원가입
+			System.out.println("UserController>join");
+			
+			//파라미터 꺼내기*4
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			String name = request.getParameter("name");
+			String gender = request.getParameter("gender");
+			
+			System.out.println(id);
+			System.out.println(password);
+			System.out.println(name);
+			System.out.println(gender);
 		}
+		
+		
+	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
